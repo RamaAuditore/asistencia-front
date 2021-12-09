1322ce614901eefa078372819ad648005db225cc
Rama <ramirobaqueiro@hotmail.com>
2021-12-09 00:44:48 -0300

    1er commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,103 @@
-import React from 'react';
-import {Formulario,Label,GrupoInput,Input,LeyendaError,IconoValidacion} from './elementos/Formularios';
+import React,{useState} from 'react';
+import {Formulario,ContenedorBotonCentrado,Boton,MensajeError,MensajeExito} from './elementos/Formularios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
-
+import {faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import Input from './componentes/Input';
  const App = () => {
+     const[nombre,cambiarNombre] = useState({campo:'', valido:null});
+     const[dni,cambiarDni] = useState({campo:'', valido:null});
+     const[clave,cambiarClave] = useState({campo:'', valido:null});
+     const[repitaclave,cambiarClave2] = useState({campo:'', valido:null});
+     const[roles,cambiarRoles] = useState({campo:'', valido:null});
+     const expresiones = {
+        nombre: /^[a-zA-ZÀ-ÿ\s]{1,22}$/, // Letras y espacios, pueden llevar acentos.
+        dni:/^.{10}$/, //Numeros
+        clave: /^[a-zA-Z0-9]{4,16}$/, // Letras, numeros.
+        roles: /^[a-z]{5,7}$/, // Letras
+    }
+    const validarClave2 =() =>{
+        if(clave.campo.length > 0){
+            if(clave.campo !== repitaclave.campo){
+                cambiarClave2((prevState) =>{
+                    return{...prevState,valido:'false'}
+                });
+            } else{
+                cambiarClave2((prevState) =>{
+                    return{...prevState,valido:'true'}
+                });
+            }
+
+            
+        }
+    }
      return ( 
          <main>
              <Formulario action="">
-             <div>
-                 <Label htmlFor="">Usuario</Label>
-                 <GrupoInput>
-                 <Input type="texts" placeholder="Usuario"/>
-                  <IconoValidacion icon={faCheckCircle}/>
-                 </GrupoInput>
-                 <LeyendaError>Lorem ipsum dolor sit amet</LeyendaError>
-             </div>
-             <div>
-                 <p><FontAwesomeIcon icon={faExclamationTriangle}/><b>Error:</b>Por favor rellena el formulario correctamente</p>
-             </div>
-             <div>
-                 <button type="submit">Ingresar</button>
-                 
-             </div>
+                <Input
+                  estado={nombre}
+                  cambiarEstado={cambiarNombre}
+                  tipo="text"
+                  label="Nombre"
+                  placeholder="Ingrese su Nombre"
+                  name="nombre"
+                  leyendaError="El Nombre tiene que ser de 4 a 16 digitos y solo puede contener letras"
+                  expresionRegular={expresiones.nombre}
+                />
+                <Input
+                estado={dni}
+                cambiarEstado={cambiarDni}
+                tipo="text"
+                label="DNI"
+                placeholder="Documento Nacional de Identidad"
+                name="dni"
+                leyendaError="El DNI debe contener no mas de 8 numeros y debe incluir ."
+                expresionRegular={expresiones.dni}
+              />
+               <Input
+                estado={clave}
+                cambiarEstado={cambiarClave}
+                tipo="password"
+                label="clave"
+                placeholder="clave"
+                name="clave"
+                leyendaError="la clave puede contener de 6 a 12 caracteres entre Letras y Numeros"
+                expresionRegular={expresiones.clave}
+              />
+              <Input
+                estado={repitaclave}
+                cambiarEstado={cambiarClave2}
+                tipo="password"
+                label="repetir clave"
+                placeholder="repita la clave"
+                name="repetir clave"
+                leyendaError="la clave no es la misma"
+                expresionRegular={expresiones.clave}
+                funcion={validarClave2}
+              />
+              <Input
+                estado={roles}
+                cambiarEstado={cambiarRoles}
+                tipo="text"
+                label="roles"
+                placeholder="inserte Rol"
+                name="Roles"
+                leyendaError="el rol solo puede ser Admin o Student"
+                expresionRegular={expresiones.clave}
+              />
+              
+                
+
+             {false && <MensajeError>
+                 <p>
+                     <FontAwesomeIcon icon={faExclamationTriangle}/>
+                     <b>Error:</b>Por favor rellena el formulario correctamente
+                     </p>
+             </MensajeError>}
+             <ContenedorBotonCentrado>
+                 <Boton type="submit">Ingresar</Boton>
+                 <MensajeExito>ingresado correctamente</MensajeExito>         
+             </ContenedorBotonCentrado>
+             
              </Formulario>
          </main>
      );
--- src/elementos/Formularios.js
+++ src/elementos/Formularios.js
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled,{css} from "styled-components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const colores = {
     borde: "#0075FF",
-    error: "bb2929",
+    error: "#BB2929",
     enviado: "#1ed12d"
 }
 
@@ -21,6 +21,10 @@ font-weight: 700;
 padding 10px;
 min-height:40px;
 cursor:pointer;
+${props => props.valido === 'false' && css`
+   color: ${colores.error};
+     
+    `}
 `;
 const GrupoInput = styled.div`
  position: relative;
@@ -39,12 +43,25 @@ const Input = styled.input`
         border: 3px solid ${colores.borde};
         outline: none;
         box-shadow:3px 0px 30px rgba(163,163,163, 0.4);
-    }`;
+    }
+    ${props => props.valido === 'true' && css`
+    border: 3px solid transparent;  
+    `}
+    ${props => props.valido === 'false' && css`
+    border: 3px solid ${colores.error} !important;  
+    `}
+`;
 const LeyendaError = styled.p`
     font-size:12px;
     margin-bottom:0;
     color: ${colores.error};
-    display:none; 
+    display:none;
+    ${props => props.valido === 'true' && css`
+       display: none; 
+    `}
+    ${props => props.valido === 'false' && css`
+     display: block;  
+    `} 
 `;
 const IconoValidacion = styled(FontAwesomeIcon)`
 position: absolute;
@@ -52,7 +69,63 @@ right:10px;
 bottom:14px;
 z-index:10;
 font-size:16px;
+opacity:0;
+${props => props.valido === 'false' && css`
+  opacity: 1;
+  color:${colores.error};
+`}
+${props => props.valido === 'true' && css`
+  opacity: 1;
+  color:${colores.enviado};
+`}
 `;
+const ContenedorBotonCentrado = styled.div`
+display:flex;
+flex-direction: column;
+align-items: center;
+grid-column: span 2;
+`;
+const Boton = styled.button`
+height: 45px;
+line-height: 45px;
+width 30%;
+background: #000;
+color: #fff;
+font-weight: bold;
+border: none;
+border-radius: 3px;
+cursor: pointer;
+transition: .1s ease all;
+ &:hover{box-shadow: 3px 0px 30px rgba(163,163,163,1);
+}
+`;
+const MensajeExito = styled.p`
+font-size: 14px;
+color: ${colores.enviado};
+display:none;`;
+
+const MensajeError = styled.div`
+height:45px;
+line-height:45px;
+background:${colores.error};
+padding: 0px 15px;
+border-radius: 3px;
+grid-column: span 2;
+p{
+    margin:0;
+}
+b{
+    margin-left:6px;
+}`;
 
 
-export{Formulario,Label,GrupoInput,Input,LeyendaError,IconoValidacion};
+export{Formulario
+    ,Label
+    ,GrupoInput
+    ,Input,
+    LeyendaError
+    ,IconoValidacion
+    ,ContenedorBotonCentrado
+    ,Boton
+    ,MensajeExito
+    ,MensajeError};
